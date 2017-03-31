68dc3738dd3dd315a557123b5d8282aaaec81658
dahye Jeong <wjdekgp1750@naver.com>
2017-03-31 14:17:45 +0900

    일부 수정

--- app/controllers/lotto_controller.rb
+++ app/controllers/lotto_controller.rb
@@ -7,20 +7,20 @@ class LottoController < ApplicationController
 
   def pick_and_check
   	# page에 해당 웹 페이지를 열어서 저장. 
-	# page = open('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=') 
+  	# page = open('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=') 
 
-	# lotto_info 에 page 내용 (JSON 형식의 data) 을 읽어서 저장.
-	# lotto_info = page.read  
+  	# lotto_info 에 page 내용 (JSON 형식의 data) 을 읽어서 저장.
+  	# lotto_info = page.read  
 
-	# lotto_hash 에 JSON 형식인 lotto_info 를 Hash 로 파싱(변환)하여 저장.
-	# lotto_hash = JSON.parse(lotto_info)    
+  	# lotto_hash 에 JSON 형식인 lotto_info 를 Hash 로 파싱(변환)하여 저장.
+  	# lotto_hash = JSON.parse(lotto_info)    
 
-	# puts lotto_hash    #=> JSON 데이터가 Hash 로 변환되어 출력.
+  	# puts lotto_hash    #=> JSON 데이터가 Hash 로 변환되어 출력.
 
-	# 위의 주석된 코드들을 1줄로 줄인 코드! get_info에 hash가 저장되어있음.
-	get_info = JSON.parse open('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=').read
+  	# 위의 주석된 코드들을 1줄로 줄인 코드! get_info에 hash가 저장되어있음.
+  	get_info = JSON.parse open('http://www.nlotto.co.kr/common.do?method=getLottoNumber&drwNo=').read
 
-	drw_numbers = []		# 이번주 당첨번호
+  	drw_numbers = []		# 이번주 당첨번호
     get_info.each do |k, v|
        drw_numbers << v if k.include? 'drwtNo'
     end
@@ -43,7 +43,6 @@ class LottoController < ApplicationController
 
 
     # 인스턴스에 변수를 저장해주세요!
-    @my_numbers = my_numbers
     @drw_numbers = drw_numbers
     @bonus_number = bonus_number
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get 'lotto/index' 를 root경로로 바꿔주세요.
   get 'lotto/index' 
-  get 'result' => 'lotto#pick_and_check'
+  get 'lotto/result' => 'lotto#pick_and_check'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
